698ebad95bb0be23ed191dd051432e855c9d4180
Vanshika Rathore <vanshikarathore280@gmail.com>
2026-06-29 19:42:31 +0530

    fix: added global axios interceptor for 401 token expiry

--- hrm-frontend/src/main.jsx
+++ hrm-frontend/src/main.jsx
@@ -1,7 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.clear();
+      window.location.href = '/login';
+    }
+    return Promise.reject(error);
+  }
+);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
